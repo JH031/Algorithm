984867b0977fb276ed9d78bdd18393a34df7a06b
Juhyun Park <127715584+JH031@users.noreply.github.com>
2024-07-19 23:22:57 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 4.53 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// numbers_len은 배열 numbers의 길이입니다.
+int solution(int numbers[], size_t numbers_len) {
+    int max=-100000000;
+    for(int i=0;i<numbers_len-1;i++){
+        for(int j=i+1;j<numbers_len;j++){
+            if(numbers[i]*numbers[j]>max) max= numbers[i]*numbers[j];
+        }
+    }
+    return max;
+}
