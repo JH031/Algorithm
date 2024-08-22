28b0f04817b7da1ed71ecb2023c9d61b2cc46e4f
Juhyun Park <127715584+JH031@users.noreply.github.com>
2024-08-22 23:53:04 +0900

    [Bronze III] Title: 학점계산, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+  char score[3];
+  scanf("%s",score);
+
+  if(score[0]=='A'){
+    if(score[1]=='+') printf("4.3");
+    else if(score[1]=='-') printf("3.7");
+    else printf("4.0");
+  }
+  else if(score[0]=='B'){
+    if(score[1]=='+') printf("3.3");
+    else if(score[1]=='-') printf("2.7");
+    else printf("3.0");
+  }
+  else if(score[0]=='C'){
+    if(score[1]=='+') printf("2.3");
+    else if(score[1]=='-') printf("1.7");
+    else printf("2.0");
+  }
+  else if(score[0]=='D'){
+    if(score[1]=='+') printf("1.3");
+    else if(score[1]=='-') printf("0.7");
+    else printf("1.0");
+  }
+  else printf("0.0");
+}
