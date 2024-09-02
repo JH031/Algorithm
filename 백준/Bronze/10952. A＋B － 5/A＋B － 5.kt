ecc8e23fdba64453e07432fd7e1be5711b2fedca
Juhyun Park <127715584+JH031@users.noreply.github.com>
2024-09-02 21:03:36 +0900

    [Bronze V] Title: A+B - 5, Time: 108 ms, Memory: 13484 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.kt
@@ -0,0 +1,11 @@
+import	java.util.*
+fun	main()	{
+        val	scanner	=	Scanner(System.`in`)
+        while	(true)	{
+                val	a	=	scanner.nextInt()
+                val	b	=	scanner.nextInt()
+
+                if(a==0 && b==0) break
+                else println("${a+b}")
+        }
+}
