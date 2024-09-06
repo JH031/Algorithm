8a12087c8817c2d0a5b05d9639b93560beba4711
Juhyun Park <127715584+JH031@users.noreply.github.com>
2024-09-06 09:46:27 +0900

    [Bronze II] Title: 숫자, Time: 16 ms, Memory: 1116 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.c
+++ 백준/Bronze/10093. 숫자/숫자.c
@@ -3,24 +3,24 @@
 #include <string.h>
 
 int main(void) {
-  long long a,b;
+  long long a,b,i;
   scanf("%lld %lld",&a,&b);
   
-  if(a==b){
-    printf("%d",0);
-    return 0;
+  if(a<b){
+    printf("%lld\n",b-a-1);
+
+    for(i=a+1;i<b;i++){
+      printf("%lld ",i);
+    }
   }
+    
   else if(a>b){
-    int temp=a;
-    a=b;
-    b=temp;
+    printf("%lld\n",a-b-1);
+
+    for(i=b+1;i<a;i++){
+      printf("%lld ",i);
+    }
   }
-
-  printf("%lld\n",b-a-1);
-
-  for(int i=a+1;i<b;i++){
-    printf("%d ",i);
+  else printf("0");
+  return 0;
   }
-
-  
-}
